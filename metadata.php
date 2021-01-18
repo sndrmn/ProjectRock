bc896c352593c9bccb77f3ed5620d75d7904f131
Sander <35436862+sndrmn@users.noreply.github.com>
2021-01-18 23:01:50 +1100

    commit

--- metadata.php
+++ metadata.php
@@ -28,7 +28,7 @@
      } else {
          $local_hostname = gethostname();
          $vmtools = exec("vmware-toolbox-cmd -v");
-         $serverip = $_SERVER['SERVER_ADDR'];
+         #$serverip = $_SERVER['SERVER_ADDR'];
          echo nl2br("<strong>This VM is running On-Prem </strong> \n\n <strong> IP ADDRESS: </strong> \n $serverip \n\n <strong> HOSTNAME: </strong> \n $local_hostname \n\n <strong>VMTOOLS: </strong> \n $vmtools");
          $onprem = "VMware";
          return $onprem;
