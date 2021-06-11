2af811a60d39a83db3aa130f181209fd496c55c3
sndrmn <35436862+sndrmn@users.noreply.github.com>
2021-06-11 12:12:28 +1000

    commit

--- metadata.php
+++ metadata.php
@@ -4,20 +4,20 @@
    public function metadata_query($url){
      $curlresult = exec("curl -m 3 http://$url", $outcome, $status);
      if (0 == $status) {
-         $location = exec("curl -H Metadata:true \"http://$url./metadata/instance/compute/location?api-version=2019-03-11&format=text\"");
+         $location = exec("curl -H Metadata:true \"http://$url/metadata/instance/compute/location?api-version=2019-03-11&format=text\"");
            if ($location == 'australiaeast' Or $location == 'southeastasia'){
-               $name = exec("curl -H Metadata:true \"http://$url./metadata/instance/compute/name?api-version=2017-08-01&format=text\"");
-               $public_ip = exec("curl -H Metadata:true \"http://$url./metadata/instance/network/interface/0/ipv4/ipAddress/0/publicIpAddress?api-version=2017-08-01&format=text\"");
-               $private_ip = exec("curl -H Metadata:true \"http://$url./metadata/instance/network/interface/0/ipv4/ipAddress/0/privateIpAddress?api-version=2017-08-01&format=text\"");
-               $rgname = exec("curl -H Metadata:true \"http://$url./metadata/instance/compute/resourceGroupName?api-version=2017-08-01&format=text\"");
+               $name = exec("curl -H Metadata:true \"http://$url/metadata/instance/compute/name?api-version=2017-08-01&format=text\"");
+               $public_ip = exec("curl -H Metadata:true \"http://$url/metadata/instance/network/interface/0/ipv4/ipAddress/0/publicIpAddress?api-version=2017-08-01&format=text\"");
+               $private_ip = exec("curl -H Metadata:true \"http://$url/metadata/instance/network/interface/0/ipv4/ipAddress/0/privateIpAddress?api-version=2017-08-01&format=text\"");
+               $rgname = exec("curl -H Metadata:true \"http://$url/metadata/instance/compute/resourceGroupName?api-version=2017-08-01&format=text\"");
                $local_hostname = gethostname();
                $OS = php_uname();
                $WebServer = $_SERVER['SERVER_SOFTWARE'];
                echo nl2br("<strong>This VM is running in Azure</strong> \n\n <strong>VM Name:</strong> \n$name \n\n<strong>PRIVATE IP:</strong> \n$private_ip \n\n<strong>HOSTNAME:</strong> \n$local_hostname \n\n<strong>OS VERSION:</strong> \n$OS \n\n<strong>WEB SOFTWARE:</strong> \n$WebServer \n\n <strong>Location:</strong> \n$location \n\n <strong>Public IP:</strong> \n$public_ip \n\n <strong>RESOURCE GROUP:</strong> \n $rgname");
            } else {
-              $instance_id = exec("curl http://$url./latest/meta-data/instance-id");
-              $reg_az = exec("curl http://$url./latest/meta-data/placement/availability-zone/");
-              $public_ipv4 = exec("curl http://$url./latest/meta-data/public-ipv4/");
+              $instance_id = exec("curl http://$url/latest/meta-data/instance-id");
+              $reg_az = exec("curl http://$url/latest/meta-data/placement/availability-zone/");
+              $public_ipv4 = exec("curl http://$url/latest/meta-data/public-ipv4/");
               $local_hostname = gethostname();
               $OS = php_uname();
               $WebServer = $_SERVER['SERVER_SOFTWARE'];
